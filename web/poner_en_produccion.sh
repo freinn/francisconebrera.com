3047ec2e6b8f80ca56aa913867ae5e7c063b24a9
freinn <freinn@gmail.com>
2017-05-07 16:27:21 +0100

    mejor

--- web/poner_en_produccion.sh
+++ web/poner_en_produccion.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-source ~/.bashrc
-ROCKET_ENV=production && ROCKET_PORT=80 && `cargo run`
+# source ~/.bashrc
+
+# sudo erases the environment variables that you've set
+# -E preserves them
+
+ROCKET_ENV=production ROCKET_PORT=80 sudo -E /home/freinn/.cargo/bin/cargo run
